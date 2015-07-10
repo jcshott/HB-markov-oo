dc6c9c8fb0b30beb4f5e26ddb4e765380159d20b
Hackbright Student <info@hackbrightacademy.com>
2015-07-09 18:03:11 -0700

    added Twitter Generator class and got MotherTSwitft text

--- markov.py
+++ markov.py
@@ -4,6 +4,8 @@ import random
 
 class SimpleMarkovGenerator(object):
 
+    char_count = 500
+
     def read_files(self, filenames):
         """Given a list of files, make text (the corpus) from them."""
 
@@ -40,6 +42,7 @@ class SimpleMarkovGenerator(object):
     def make_text(self, chains):
         """Takes dictionary of markov chains; returns random text."""
         # Grab random key from markov chain dict to start off the generated text
+        #print self.char_count
         key_list = chains.keys()
             #  start_index = random.randint(0, (len(key_list) - 1))
         start_key = random.choice(key_list)
@@ -69,17 +72,26 @@ class SimpleMarkovGenerator(object):
                 temp_key += (key[n],)
             # print "next word", next_word
             key = temp_key + (next_word,)
-        
+
+        temp_markov_string = " ".join(random_list_for_string)
+        final_markov_string = temp_markov_string[:self.char_count]
+       
+        #print final_markov_string
     # print random_list_for_string
-        final_list_for_string = []
-        for search_i in xrange(len(random_list_for_string) - 1, 0, -1):
-            if random_list_for_string[search_i][-1] == "." or random_list_for_string[search_i][-1] == "!" or random_list_for_string[search_i][-1] == "?":
-                final_list_for_string = random_list_for_string[:search_i + 1]
         
-        markov_string = " ".join(final_list_for_string)
+        
+        for search_i in xrange(len(final_markov_string) - 1, 0, -1):
+            if final_markov_string[search_i][-1] == "." or final_markov_string[search_i][-1] == "!" or final_markov_string[search_i][-1] == "?":
+                final_markov_string = final_markov_string[:search_i + 1]
+        
+        
 
-        return markov_string 
+        return final_markov_string 
 
+
+class TwitterMarkovGenerator(SimpleMarkovGenerator):
+    """creates a tweet (140 character) sized Markov chain"""
+    char_count = 300
 
 if __name__ == "__main__":
 
@@ -89,11 +101,11 @@ if __name__ == "__main__":
         filenames.append(sys.argv[i])
 
     # we should make an instance of the class
-    markov = SimpleMarkovGenerator()
+    markov = TwitterMarkovGenerator()
 
     # we should call the read_files method with the list of filenames
     text = markov.read_files(filenames)
     markov_chains = markov.make_chains(text, 2)
     # we should call the make_text method 5x
-    for i in xrange(5):
-        print markov.make_text(markov_chains)
+    #for i in xrange(5):
+    print markov.make_text(markov_chains)
